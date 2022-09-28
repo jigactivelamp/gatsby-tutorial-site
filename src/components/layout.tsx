e9642ad3318c4750b11b7f55debb338ebd41951e
Jig James Marababol <jig@activelamp.com>
2022-09-28 12:36:30 +0800

    Finished Gatsby Tutorial Part 2

---
+++ src/components/layout.tsx
@@ -0,0 +1,42 @@
+import * as React from "react";
+import { Link } from "gatsby";
+const {
+  container,
+  heading,
+  navLinks,
+  navLinkItem,
+  navLinkText,
+} = require("./layout.module.css");
+
+const Layout = ({
+  pageTitle,
+  children,
+}: {
+  pageTitle: string;
+  children: JSX.Element;
+}) => {
+  return (
+    <div className={container}>
+      <nav>
+        <ul className={navLinks}>
+          <li className={navLinkItem}>
+            <Link to="/" className={navLinkText}>
+              Home
+            </Link>
+          </li>
+          <li className={navLinkItem}>
+            <Link to="/about" className={navLinkText}>
+              About
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <main>
+        <h1 className={heading}>{pageTitle}</h1>
+        {children}
+      </main>
+    </div>
+  );
+};
+
+export default Layout;
---
+++ src/pages/sample.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function SampleComponent() {
+  return (
+    <>
+      <div>
+        <h1>A valid component!</h1>
+        <p>This will work fine.</p>
+        <p>Since there is only one top-level element: the div.</p>
+      </div>
+    </>
+  );
+}
